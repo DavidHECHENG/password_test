5c8ae72ed26f7539ff5a63adf29397f7195f92fb
HE CHENG <as0960311360@gmail.com>
2024-02-21 14:55:22 +0200

    password_test program

---
+++ password.py
@@ -0,0 +1,12 @@
+x = 3
+while True :
+    password = input('請輸入密碼：')
+    if password == 'lovezoe' :
+        print('恭喜答對！')
+        break
+    elif password != 'lovezoe' and x >= 0 :
+        x = x - 1
+        if x == 0 :
+            print('密碼錯誤！您已使用完所有機會')
+            break
+        print('密碼錯誤！您還有',x,'次機會')
