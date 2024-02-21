cec4fa4267e4336b5a294abfa20a39ff0fc6272e
HE CHENG <132820819+DavidHECHENG@users.noreply.github.com>
2024-02-21 14:59:13 +0200

    Add files via upload

--- password.py
+++ password.py
@@ -1,10 +1,11 @@
 x = 3
+pwd = 'lovezoe'
 while True :
     password = input('請輸入密碼：')
-    if password == 'lovezoe' :
+    if password == pwd :
         print('恭喜答對！')
         break
-    elif password != 'lovezoe' and x >= 0 :
+    elif password != pwd and x >= 0 :
         x = x - 1
         if x == 0 :
             print('密碼錯誤！您已使用完所有機會')
